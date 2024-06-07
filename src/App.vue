808f75716ec8fa9f8b806f70905264ecae0fc197
没了酒的李白 <8449554+txp2039442945@user.noreply.gitee.com>
2024-06-07 16:04:17 +0800

    修改app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view /> -->
+  <div>loser</div>
 </template>
 
 <style lang="scss">
